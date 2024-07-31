8a58d58d726bcabfcd05bd8a8e163189aed60c7a
Daniel Omekam <omekam.daniel@gmail.com>
2024-07-30 21:06:51 -0400

    create index.html

---
+++ vault.js
@@ -0,0 +1,13 @@
+//Pseudocode: create a vault combination of 10 - 40 - 39 using 3 different arithmetic operators that will generate each individual number
+
+let str1 = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+let num1 = 9 + 1;
+let num2 = 2 * 30 - 20;
+let num3 = 78/2;
+
+let vault_code = `${str1} ${num1} - ${num2} - ${num3}`;
+
+alert(vault_code);
+
+
+
